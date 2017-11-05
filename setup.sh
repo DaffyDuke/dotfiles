9a2bd8de6e79303488da0d379d5f28f12a3dc865
Olivier Duquesne <daffyduke@lautre.net>
2017-11-05 22:24:07 +0100

    cosmetic removal

--- setup.sh
+++ setup.sh
@@ -91,11 +91,10 @@ sudo apt-get install docker-ce
 
 # Install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
 vim +BundleInstall
 vim +GoInstallBinaries
-# Install tmux
-#  Install oh-my-szh
+
+# Install oh-my-szh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # INstall eslint for syntastic
 sudo npm install -g eslint
