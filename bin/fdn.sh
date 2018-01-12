e629618fad1fbb75755111c28e721064c86da4db
Olivier Duquesne <daffyduke@lautre.net>
2018-01-12 21:47:51 +0100

    do not break link

--- bin/fdn.sh
+++ bin/fdn.sh
@@ -2,16 +2,27 @@
 
 case $1 in
     'start')
-        sudo mv /etc/resolv.conf /etc/resolv.conf-orig
-        sudo cp /etc/resolv.conf-fdn /etc/resolv.conf
+        sudo rm -f /etc/resolv.conf 2>/dev/null
+        echo "nameserver 80.67.169.12" | sudo tee /etc/resolv.conf
+        echo "nameserver 80.67.169.40" | sudo tee -a /etc/resolv.conf
         ;;
     'stop')
-        sudo rm /etc/resolv.conf
-        sudo mv /etc/resolv.conf-orig /etc/resolv.conf
+        sudo ln -sf /run/resolvconf/resolv.conf /etc/resolv.conf
         ;;
     'status')
-        ls -l /etc/resolv.conf
-        cat /etc/resolv.conf
+        if [ -h /etc/resolv.conf ]
+        then
+          echo "managed by NetworkManager"
+        else
+          echo -e "Dedicated file : \c"
+          if [ $(grep -c 80.67.169 /etc/resolv.conf) -eq 2 ]
+          then
+            echo FDN
+          else
+            echo OTHERS
+            cat /etc/resolv.conf
+          fi
+        fi
         ;;
     *)
         echo 'valid argument : start|stop|status'
