d165109480464b682f8afeec339bfaf534f8720f
Olivier Duquesne <daffyduke@lautre.net>
2018-02-19 07:45:45 +0100

    add dockstation : https://github.com/DockStation/dockstation/

--- setup.sh
+++ setup.sh
@@ -128,7 +128,10 @@ sudo apt-get install apt-transport-https ca-certificates curl software-propertie
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce docker-compose
+cd /tmp
+wget https://github.com/DockStation/dockstation/releases/download/v1.3.0/dockstation_1.3.0_amd64.deb
+sudo dpkg -i /tmp/dockstation_1.3.0_amd64.deb
 
 # TLDR
 go get 4d63.com/tldr
