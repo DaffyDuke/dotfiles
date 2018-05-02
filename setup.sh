228e1737828d133a399dbc570b20b9ab886d2938
Olivier Duquesne <daffyduke@lautre.net>
2018-05-02 13:15:07 +0200

    adapt some bionic from nearly sracthed distrib ....

--- setup.sh
+++ setup.sh
@@ -4,53 +4,49 @@
 sudo timedatectl set-local-rtc 1
 
 # https://itsfoss.com/things-installing-ubuntu-17-10/
-sudo apt update && sudo apt upgrade
-sudo apt install tlp tlp-rdw
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y tlp tlp-rdw
 sudo tlp start
 
 # this tool aims to be ran from Ubuntu
 # install additionnal repositories
 for ppa in ppa:djcj/hybrid \
-  ppa:eviltwin1/feedreader-stable \
   ppa:geary-team/releases \
   ppa:gezakovacs/ppa \
   ppa:hollywood/ppa \
-  ppa:mariospr/frogr \
   ppa:pbek/qownnotes \
   ppa:peek-developers/stable \
   ppa:peterlevi/ppa \
   ppa:teejee2008/ppa \
   ppa:unit193/encryption \
-  ppa:uroni/urbackup \
   ppa:vincent-vandevyvre/vvv \
   ppa:webupd8team/indicator-kdeconnect \
-  ppa:webupd8team/tor-browser \
   ppa:webupd8team/y-ppa-manager \
   ppa:twodopeshaggy/jarun \
   ppa:yubico/stable \
   ppa:libreoffice/ppa \
   ppa:yannubuntu/boot-repair
 do
-  sudo apt-add-repository --yes ${ppa}
+  sudo apt-add-repository -n --yes ${ppa}
 done
 sudo apt-get update
-sudo apt install -y cool-retro-term hollywood frogr qownnotes peek variety timeshift veracrypt urbackup oqapy indicator-kdeconnect tor-browser y-ppa-manager ddgr python-software-properties boot-repair
+sudo apt install -y hollywood qownnotes peek variety timeshift veracrypt oqapy indicator-kdeconnect y-ppa-manager ddgr software-properties-common boot-repair # frogr urbackup cool-retro-term bluegriffon bcc-tools freemind harmony lightworks youtube-dlg
 sudo apt install -y git zsh expect
 
 # Install some packages
 sudo apt install -y \
   alien androidsdk-ddms ansible ansible-lint apt-file atop awscli \
-  baobab bc bcc-tools bluegriffon build-essential bundler \
-  ca-certificates checkinstall clipit corkscrew cowsay cpuid \ # ctop
+  baobab bc build-essential bundler \
+  ca-certificates checkinstall clipit corkscrew cowsay cpuid curl \
   darktable debian-goodies default-jre deluge-gtk dfc dnstracer dos2unix \
   easytag ethstatus ethtool ettercap-graphical evince \
-  fastboot filezilla fortunes-fr freemind ftp \
-  gcompris gcompris-sound-fr gcstar geary glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent google-cloud-sdk graphviz guake guake-indicator \
-  handbrake harmony heimdall-flash-frontend htop httpcode httperf httpie hugin hugo \
+  fastboot filezilla fortunes-fr ftp \
+  gcompris gcstar geary glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent google-cloud-sdk graphviz guake guake-indicator \
+  handbrake heimdall-flash-frontend htop httpcode httperf httpie hugin hugo \
   icedtea-plugin iftop ioping iotop iproute2 iptraf iputils-arping iptstate \
   jq junior-programming jxplorer \
   keepass2 kerneloops klavaro \
-  language-pack-fr ldap-utils lftp libpam-yubico libreoffice-gnome lightworks lolcat lsof ltrace lynx \
+  language-pack-fr ldap-utils lftp libpam-yubico libreoffice-gnome lolcat lsof ltrace lynx \
   mat mc meld ncdu mono-complete \
   netcat-openbsd nethogs nmap nmon numatop \
   ogmrip openconnect openssh-client openssh-server openvpn owncloud-client \
@@ -58,12 +54,12 @@ sudo apt install -y \
   qarte qemu qtpass \
   rdesktop remmina repo rpm rsync \
   s3cmd scribus seahorse scdaemon shellcheck shotwell simple-scan sosreport spectre-meltdown-checker sshfs sshpass strace stunnel4 synaptic sysstat \
-  tcpdump testssl.sh thefuck tilix toilet traceroute tshark tuxmath \
+  tcpdump testssl.sh thefuck tilix toilet torbrowser-launcher traceroute tshark tuxmath \
   ubuntu-restricted-extras unetbootin unrar \
   vagrant vim-gnome vim-youcompleteme virt-manager vlc \
   weboob-qt whois wireshark \
   xauth xscreensaver \
-  yamllint youtube-dlg yubikey-manager-qt \
+  yamllint yubikey-manager-qt \
   zenmap
 sudo apt remove gnome-screensaver
 mkdir -p ~/.config/systemd/user/
@@ -79,14 +75,16 @@ sudo apt-get install -y atom
 
 # Dropbox
 # https://www.dropbox.com/install-linux
-wget -o /tmp/dropbox_2015.10.28_amd64.deb -O https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+wget -O /tmp/dropbox_2015.10.28_amd64.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
 sudo dpkg -i /tmp/dropbox_2015.10.28_amd64.deb
+sudo apt --fix-broken install
 
 # VirtualBox
 # https://www.virtualbox.org/wiki/Linux_Downloads
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-echo "deb http://download.virtualbox.org/virtualbox/debian yakkety contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+sudo rm /etc/apt/sources.list.d/virtualbox.list
+echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
 sudo apt-get install virtualbox-5.2
 
@@ -98,13 +96,14 @@ sudo apt-get install -f
 
 # Spotify
 # https://doc.ubuntu-fr.org/spotify
+sudo rm /apt/sources.list.d/spotify.list
 sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
-udo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
-udo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EFDC8610341D9410
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EFDC8610341D9410
 sudo apt-get update
 sudo apt-get install spotify-client
 
-# Slack
+# FIXME Slack
 # https://slack.com/intl/fr-fr/downloads/Linux
 curl -s https://packagecloud.io/install/repositories/slacktechnologies/slack/script.deb.sh | sudo bash
 
@@ -119,10 +118,9 @@ sudo apt-get install google-chrome-stable
 # Puppet
 # https://docs.puppet.com/puppet/4.5/install_linux.html#installing-release-packages-on-apt-based-systems
 wget http://apt.puppetlabs.com/puppet-release-xenial.deb;
-sudo dpkg -i puppet-release-xenial.deb
-sudo apt-get update
+sudo dpkg -i puppet-release-xenial.deb || rm -f puppet-release-xenial.deb
 
-# Docker
+# FIXME Docker
 # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
 sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
@@ -130,9 +128,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-compose
-cd /tmp
+cd /tmp || exit
 wget https://github.com/DockStation/dockstation/releases/download/v1.3.0/dockstation_1.3.0_amd64.deb
-sudo dpkg -i /tmp/dockstation_1.3.0_amd64.deb
+sudo dpkg -i /tmp/dockstation_1.3.0_amd64.deb && rm /tmp/dockstation_1.3.0_amd64.deb
 
 # TLDR
 go get 4d63.com/tldr
@@ -144,33 +142,29 @@ vim +GoInstallBinaries
 
 # Install oh-my-szh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# INstall eslint for syntastic
-sudo npm install -g eslint
-sudo npm install -g babel-eslint
-sudo npm install -g eslint-plugin-react
 
 # XAuth
 # https://superuser.com/questions/806637/xauth-not-creating-xauthority-file/941244#941244
- rm .Xauthority
+ rm -f .Xauthority
  # xauth with complain unless ~/.Xauthority exists
  touch ~/.Xauthority
  # only this one key is needed for X11 over SSH 
  xauth generate :0 . trusted 
  # generate our own key, xauth requires 128 bit hex encoding
- xauth add ${HOST}:0 . $(xxd -l 16 -p /dev/urandom)
+ xauth add "${HOST}":0 . $(xxd -l 16 -p /dev/urandom)
  # To view a listing of the .Xauthority file, enter the following 
  xauth list 
 
  # Install Tilix Theme
  mkdir -p ~/.config/tilix/schemes/
- wget -qO $HOME"/.config/tilix/schemes/desert.json" https://raw.githubusercontent.com/storm119/Tilix-Themes/master/Themes/desert.json
+ wget -qO "${HOME}"/.config/tilix/schemes/desert.json https://raw.githubusercontent.com/storm119/Tilix-Themes/master/Themes/desert.json
  gsettings set org.gnome.desktop.default-applications.terminal exec 'terminal'
 
-# Fix ttf-mscorefonts-installer
+# Fixed in bionic ttf-mscorefonts-installer
 # http://www.asso-linux.org/forum/viewtopic.php?f=4&t=196
-wget -o /tmp/ttf-mscorefonts-installer_3.6_all.deb -O http://ftp.fr.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
-sudo dpkg -i /tmp/ttf-mscorefonts-installer_3.6_all.deb
-sudo apt install -f
+# wget -o /tmp/ttf-mscorefonts-installer_3.6_all.deb -O http://ftp.fr.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
+# sudo dpkg -i /tmp/ttf-mscorefonts-installer_3.6_all.deb
+# sudo apt install -f
 
 # Install atom modules
 # http://flight-manual.atom.io/using-atom/sections/atom-packages/
@@ -196,6 +190,7 @@ for module in git-plus \
 do 
   apm install "${module}"
 done
+echo "apm complete"
 
 # Install Gnome-extensions
 # https://wiki.gnome.org/Projects/GnomeShell/Extensions#Enabling_extensions
@@ -224,7 +219,7 @@ dconf write /org/gnome/shell/extensions/panel-osd/x-pos 90.0
 # dconf watch / is your friend !
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gsettings set org.gnome.settings-daemon.plugins.color night-light-last-coordinates (50.633000000000003, 3.0586000000000002)
+gsettings set org.gnome.settings-daemon.plugins.color night-light-last-coordinates '(50.633000000000003, 3.0586000000000002)'
 gsettings set org.gnome.desktop.wm.preferences theme 'Radiance'
 gsettings set org.gnome.desktop.interface gtk-theme 'Radiance'
 gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-White'
@@ -261,16 +256,16 @@ gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backg
 
 # Kubernetes
 # https://github.com/kubernetes/minikube
-cd ~/bin/
+cd ~/bin/ || exit
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
 export MINIKUBE_HOME=$HOME
 export CHANGE_MINIKUBE_NONE_USER=true
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
-export KUBECONFIG=$HOME/.kube/config
+mkdir "$HOME"/.kube || true
+touch "$HOME"/.kube/config
+export KUBECONFIG="$HOME"/.kube/config
 sudo -E ./minikube start
 # this for loop waits until kubectl can access the api server that Minikube has created
 for i in {1..150}; do # timeout for 5 minutes
@@ -285,7 +280,7 @@ sudo kubectl expose deployment hello-minikube --type=NodePort
 curl $(minikube service hello-minikube --url)
 # sudo minikube dashboard
 sudo minikube stop
-cd /tmp
+cd /tmp || exit
 git clone git clone https://github.com/ahmetb/kubectx.git
 cp -v kubectx/{kubectx,kubens,utils.bash} ~/bin/ 
 # Some other tools
@@ -297,32 +292,32 @@ cp -v kubectx/{kubectx,kubens,utils.bash} ~/bin/
 # some cloud tools
 # Packer
 # https://www.packer.io/downloads.html
-cd ~/bin
+cd ~/bin || exit
 wget https://releases.hashicorp.com/packer/1.1.1/packer_1.1.1_linux_amd64.zip
 unzip packer_1.1.1_linux_amd64.zip
 rm packer_1.1.1_linux_amd64.zip
 
 # terraform
 # https://www.terraform.io/downloads.html
-cd ~/bin
+cd ~/bin || exit
 wget https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip
-unzip terraform_0.10.8_linux_amd64.zip
-rm terraform_0.10.8_linux_amd64.zip
+unzip -f terraform_0.10.8_linux_amd64.zip
+rm -f terraform_0.10.8_linux_amd64.zip getTerraformProviders.sh
 wget https://gist.githubusercontent.com/jnahelou/63947831a8154daf6bc3573cc27ed373/raw/e7c685d8ae80e3c6b17238703a870cab00edc7b0/getTerraformProviders.sh
 sudo mkdir -p /usr/local/terraform/toolbox/providers/
 sudo bash ~/bin/getTerraformProviders.sh
-cd
+cd "$HOME" || exit
 terraform init -plugin-dir=/usr/local/terraform/toolbox/providers/
 # terraform graph | dot -Tpng > terraform-graph.png
 
 # rancher
 # https://github.com/rancher/cli/release
-cd ~/bin
+cd ~/bin || exit
 wget https://github.com/rancher/cli/releases/download/v0.6.5-rc4/rancher-linux-amd64-v0.6.5-rc4.tar.gz
 tar xvfz rancher-linux-amd64-v0.6.5-rc4.tar.gz
 rm rancher-linux-amd64-v0.6.5-rc4.tar.gz
 # https://github.com/rancher/rancher-compose/releases
-cd ~/bin
+cd ~/bin || exit
 wget https://github.com/rancher/rancher-compose/releases/download/v0.12.5/rancher-compose-linux-amd64-v0.12.5.tar.gz
 tar xvfz rancher-compose-linux-amd64-v0.12.5.tar.gz
 rm rancher-compose-linux-amd64-v0.12.5.tar.gz
@@ -333,8 +328,8 @@ curl https://beyondgrep.com/ack-2.22-single-file > ~/bin/ack && chmod 0755 ~/bin
 
 # GO
 # https://golang.org/dl/
-mkdir -p $GOROOT $GOPATH
-cd $GOROOT
+mkdir -p "$GOROOT" "$GOPATH"
+cd "$GOROOT" || exit
 wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
 tar xvfz go1.9.2.linux-amd64.tar.gz
 rm go1.9.2.linux-amd64.tar.gz
@@ -355,27 +350,31 @@ curl https://packages.microsoft.com/config/ubuntu/17.10/prod.list | sudo tee /et
  # Install PowerShell
  sudo apt-get install -y powershell
 # as repo is not available for the moment ....
-cd /tmp
+cd /tmp || exit
 wget -O https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.9/powershell_6.0.0-beta.9-1.ubuntu.17.04_amd64.deb
 sudo dpkg -i powershell_6.0.0-beta.9-1.ubuntu.17.04_amd64.deb
 
 # CloudConvert
 sudo npm install -g cloudconvert-cli
-export CLOUDCONVERT_API_KEY=<changeme>
+export CLOUDCONVERT_API_KEY=changeme
 
 # Fast speedtest by Netflix commandline
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get install -y nodejs
 # sudo npm install --global fast-cli  # needs to understand why installation fail
+# INstall eslint for syntastic
+sudo npm install -g eslint
+sudo npm install -g babel-eslint
+sudo npm install -g eslint-plugin-react
 
 # Gandi Client
-cd /tmp
+cd /tmp || exit
 sudo apt-get install -y python-nose python3-nose python-ipy
 git clone https://github.com/Gandi/gandi.cli.git && cd gandi.cli
 ln -sf packages/debian debian && debuild -us -uc -b && sudo dpkg -i ../python-gandicli_1.0_all.deb
 
 # Android Rules
-cd /tmp
+cd /tmp || exit
 git clone git@github.com:M0Rf30/android-udev-rules.git
 sudo cp android-udev-rules/51-android.rules /etc/udev/rules.d/ 
 sudo chmod a+r /etc/udev/rules.d/51-android.rules
@@ -443,6 +442,12 @@ sudo apt remove cargo rustc
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 cargo install gli
+
+# Feedreader
+curl https://raw.githubusercontent.com/jangernert/FeedReader/master/scripts/install_ubuntu.sh | bash
+
+# urbackup
+TF=`mktemp` && wget "https://hndl.urbackup.org/Client/2.2.5/UrBackup%20Client%20Linux%202.2.5.sh" -O $TF && sh $TF; rm $TF
 
 # lnav
 cd ~/bin
