125bc2153f7afe54d5665d2b6fb864850f37dfce
Olivier Duquesne <daffyduke@lautre.net>
2018-06-08 16:55:31 +0200

    add gparted

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ sudo apt install -y \
   darktable debian-goodies default-jre deluge-gtk dfc dkms dnstracer dos2unix \
   easytag ethstatus ethtool ettercap-graphical evince \
   fastboot filezilla fortunes-fr ftp \
-  gcompris gcstar geary gimp glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent google-cloud-sdk graphviz gthumb guake guake-indicator \
+  gcompris gcstar geary gimp glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent google-cloud-sdk gparted graphviz gthumb guake guake-indicator \
   handbrake hddtemp heimdall-flash-frontend htop httpcode httperf httpie hugin hugo \
   icedtea-plugin iftop ioping iotop iproute2 iptraf iputils-arping iptstate \
   jq junior-programming jxplorer \
