7dca2da6b8c0baca00830c93acc3b726dc80184e
Olivier Duquesne <daffyduke@lautre.net>
2018-06-17 21:29:48 +0200

    google-chrome-stable core dumped on each run

--- setup.sh
+++ setup.sh
@@ -132,7 +132,7 @@ sudo snap install slack --classic
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-unstable
 
 # Puppet
 # https://docs.puppet.com/puppet/4.5/install_linux.html#installing-release-packages-on-apt-based-systems
