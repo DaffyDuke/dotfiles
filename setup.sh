b7ba82417a34f9210695ff88d6fbaff5be2231f5
Olivier Duquesne <daffyduke@lautre.net>
2018-08-03 02:14:09 +0200

    add network-manager-openvpn-gnome

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ sudo apt install -y \
   keepass2 kerneloops kigo klavaro \
   language-pack-fr ldap-utils lftp libpam-yubico libreoffice libreoffice-calc libreoffice-draw libreoffice-help-fr libreoffice-impress libreoffice-math libreoffice-nlpsolver libreoffice-pdfimport libreoffice-voikko libreoffice-writer libreoffice-templates libreoffice-writer2latex libreoffice-gnome lm-sensors lolcat lsof ltrace lynx \
   mat mc meld ncdu mono-complete mutt \
-  netcat-openbsd nethogs nmap nmon npm numatop \
+  netcat-openbsd nethogs network-manager-openvpn-gnome nmap nmon npm numatop \
   ogmrip openclipart-libreoffice openconnect openssh-client openssh-server openvpn owncloud-client \
   p7zip parted pass patch pcp perf-tools-unstable perl-doc pgtop photocollage pidgin pidgin-skype pidgin-hangouts pidgin-encryption pidgin-openpgp pidgin-gnome-keyring pinentry-curses pinentry-tty pidcat planfacile playonlinux postgresql-client psensor pssh putty-tools python python3 python3-pip python3-dev python3-virtualenv pwgen pydf \
   qarte qemu qtpass \
