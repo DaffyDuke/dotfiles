c9a41514c3dbd127829f717079d5ffad325ff095
Olivier Duquesne <daffyduke@lautre.net>
2018-10-01 19:43:14 +0200

    fix gcp tools

--- setup.sh
+++ setup.sh
@@ -454,7 +454,14 @@ CLOUD()
   # setup https://github.com/Caiyeon/goldfish/wiki/Production-Deployment
 
   # GCP
-  sudo apt install google-cloud-sdk
+  # Create environment variable for correct distribution
+  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+  # Add the Cloud SDK distribution URI as a package source
+  echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  # Import the Google Cloud Platform public key
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  # Update the package list and install the Cloud SDK
+  sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 }
 
 GO()
@@ -819,10 +826,10 @@ Main()
 #  bcctools
 #  bluegriffon
 #  Chaos
-#   Children
+#  Children
 #  Chrome
 #  chromeIPass
-#  CLOUD
+  CLOUD
 #  coolretroterm
 #  CozyDrive
 #  Ctop
@@ -866,7 +873,7 @@ Main()
 #  WTF
 #  XAuth
 #  youtube
-  YunoHost
+#  YunoHost
 #  ZSH
 }
 
