675a241f6707f643fa90c132967ae82eea043aed
Olivier Duquesne <daffyduke@lautre.net>
2018-12-28 02:21:07 +0100

    add gromit-mpx : https://voidlinux.org/news/2018/12/01-advent-gromit-mpx.html

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ Packages()
     darktable debian-goodies default-jre deluge-gtk deluged dfc dkms dnstracer dos2unix \
     easytag ethstatus ethtool ettercap-graphical evince evolution extrace exuberant-ctags \
     fastboot fdupes sudo apt-get install ffmpegthumbnailer filezilla fonts-powerline fortunes-fr fslint ftp \
-    gcstar geary gimp glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent gparted graphviz gthumb guake guake-indicator \
+    gcstar geary gimp glances gnome-tweak-tool gnome-usage gnupg2 gnupg-agent gparted graphviz gromit-mpx gthumb guake guake-indicator \
     handbrake hddtemp heimdall-flash-frontend htop httpcode httperf httpie hugin hugo hunspell-fr hunspell-fr-comprehensive \
     i2c-tools: icedtea-plugin iftop ioping iotop ipcalc iproute2 iptraf iputils-arping iptstate \
     jq jxplorer \
