8eea44b0c46e87d5f8f81101860f030ed7e24be2
Olivier Duquesne <daffyduke@lautre.net>
2020-02-23 01:21:59 +0100

    add tmux-plugin-spotify : http://wittchen.io/spotify-song-in-i3-status-bar/

--- setup.sh
+++ setup.sh
@@ -326,6 +326,8 @@ Spotify()
   # sudo apt-get install -y spotify-client
   sudo snap install spotify
   sudo ln -s /var/lib/snapd/desktop/applications/spotify_spotify.desktop /usr/share/applications/spotify.desktop
+  pip install spotify-cli-linux
+  pip install lyricwikia
 }
 
 Slack()
