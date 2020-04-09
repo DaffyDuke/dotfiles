6ccd2b9e47e70486c2a02f937ada755a35dd4db9
Olivier Duquesne <daffyduke@lautre.net>
2020-04-09 22:47:39 +0200

    copy-on-select true source : https://github.com/gnunn1/tilix/commit/0c2b94a1cfd138e5feb6b68e42286050fce238ac

--- setup.sh
+++ setup.sh
@@ -466,6 +466,7 @@ Terminal()
   dconf write /com/gexperts/Tilix/profiles/${default}/default-size-columns '200'
   dconf write /com/gexperts/Tilix/profiles/${default}/default-size-rows '50'
   dconf write /com/gexperts/Tilix/profiles/${default}/palette "['#4D4D4D', '#FF2B2B', '#98FB98', '#F0E68C', '#CD853F', '#FFDEAD', '#FFA0A0', '#F5DEB3', '#555555', '#FF5555', '#55FF55', '#FFFF55', '#87CEFF', '#FF55FF', '#FFD700', '#FFFFFF']"
+  dconf write /com/gexperts/Tilix/copy-on-select true
   gsettings set org.gnome.settings-daemon.plugins.media-keys terminal ""
   gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 name unset
   gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 command unset
