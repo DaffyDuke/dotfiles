348088a64d216b6214e43400ee1be6f9090277c9
Olivier Duquesne <daffyduke@lautre.net>
2020-04-18 23:06:20 +0200

    add master alias source : https://davidwalsh.name/detect-git-directory

--- bin/gpull.sh
+++ bin/gpull.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+master() {
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    git checkout master -q && git pull -q
+  else
+    hg pull && hg checkout "last(public())"
+  fi
+}
+
+
 LOCAL=~/Documents/Code/git
 for HOSTING in adullact  babolivier  codecommit  enough  framagit  github  gitlab  
 do
@@ -7,7 +16,8 @@ do
   for DIR in $(ls -1)
   do
     cd ${LOCAL}/${HOSTING}/${DIR}
-    test -d .git && git pull -q
+#    test -d .git && git pull -q
+    master
     if [[ ! $? -eq 0 ]]
     then
       echo "####### ${LOCAL}/${HOSTING}/${DIR} à vérifier #######"
