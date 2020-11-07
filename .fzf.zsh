41578dd507045fe1bda0226b4fac7d9c9e4262c7
Olivier Duquesne <daffyduke@lautre.net>
2020-11-07 22:29:22 +0100

    fix path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/daffy/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/daffy/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/home/daffy/.fzf/bin"
 fi
 
 # Auto-completion
@@ -11,4 +11,3 @@ fi
 # Key bindings
 # ------------
 source "/home/daffy/.fzf/shell/key-bindings.zsh"
-
