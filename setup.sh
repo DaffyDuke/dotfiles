a90f118751efdcccea8cb0da8f70fd704a7508b7
Olivier Duquesne <daffyduke@lautre.net>
2022-11-06 11:59:27 +0100

    📦 NEW: add Element Matrix Desktop Software

--- setup.sh
+++ setup.sh
@@ -553,6 +553,16 @@ DVD()
   sudo dpkg-reconfigure libdvd-pkg
 }
 
+Element()
+{
+  # Element : a Matrix client
+  sudo apt install -y wget apt-transport-https
+  sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+  sudo apt update
+  sudo apt install -y element-desktop
+}
+
 FlatPackages()
 {
   # misc softwares available with flatpack command
@@ -1438,10 +1448,11 @@ Main()
 #  Dropbox
 #  DroidCAM
 #  DVD
+  Element
 #  FlatPack
 #  Feedreader
 #  Fuzzy
-  GCStar
+#  GCStar
 #  Github
 #  GnomeConfigurations
 #  GnomeExtensions
