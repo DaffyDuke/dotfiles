1a6785af06e286625db42ac5d896e62d99c40e02
Olivier Duquesne <daffyduke@lautre.net>
2023-12-28 23:33:01 +0100

    📦 NEW: add kubie: https://github.com/sbstp/kubie

--- setup.sh
+++ setup.sh
@@ -1065,7 +1065,7 @@ Rust()
   rustup install stable
   rustup default stable
 
-  for rustpkg in bat delta-git dog dust dutree erdtree fd-find just gping mdcat navi ripgrep spotify-tui trippy viu
+  for rustpkg in bat delta-git dog dust dutree erdtree fd-find gping just kubie mdcat navi ripgrep spotify-tui trippy viu
   do
     cargo install "${rustpkg}"
   done
