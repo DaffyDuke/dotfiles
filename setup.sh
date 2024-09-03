37364494bce8310d5b8da4b237b701222d31f799
Olivier Duquesne <daffyduke@lautre.net>
2024-09-03 21:30:50 +0200

    📦 NEW: add dive : A tool for exploring each layer in a docker image

--- setup.sh
+++ setup.sh
@@ -542,6 +542,11 @@ Docker()
   sudo gpasswd -a $USER docker
   # https://github.com/jesseduffield/lazydocker#installation
   go get github.com/jesseduffield/lazydocker
+  # Dive https://github.com/wagoodman/dive
+  cd /tmp || exit
+  DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+  curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
+  sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
 }
 
 Douane()
