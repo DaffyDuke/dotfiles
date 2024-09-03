0f715ac68294391a8d6db58939169eeffa076765
Olivier Duquesne <daffyduke@lautre.net>
2024-09-03 16:10:45 +0200

    📦 NEW: add deface : Video anonymization by face detection

--- setup.sh
+++ setup.sh
@@ -171,7 +171,7 @@ Python()
   # python run_demo.py
 
   # Install some other pip cool stuff
-  for pkg in bcc bpytop betago configobj docopt git-pull-request grip howdoi icdiff jsonnet kapitan litecli mycli search-that-hash shodan spotify-cli-linux tenserflow terminaltables virtualenv yt-dlp
+  for pkg in bcc bpytop betago configobj deface docopt git-pull-request grip howdoi icdiff jsonnet kapitan litecli mycli search-that-hash shodan spotify-cli-linux tenserflow terminaltables virtualenv yt-dlp
   do
     pip install "${pkg}" --upgrade --break-system-packages
   done
